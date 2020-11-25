3aff068aaaa5c9df24ae5bbf5e534daebadb3b8b
milovanovic <barun@eecs.berkeley.edu>
2020-11-25 15:39:24 +0100

    AWGUtils modified

--- src/main/scala/AWGUtils.scala
+++ src/main/scala/AWGUtils.scala
@@ -85,33 +85,56 @@ class AXIBridge3to1(dataWidth: Int, csrAddress: AddressSet,beatBytes: Int) exten
     interfaceOut1.ready := ioout.ready && interfaceOut1.valid && interfaceOut2.valid && interfaceOut3.valid
     interfaceOut2.ready := ioout.ready && interfaceOut1.valid && interfaceOut2.valid && interfaceOut3.valid
     interfaceOut3.ready := ioout.ready && interfaceOut1.valid && interfaceOut2.valid && interfaceOut3.valid
-    ioout.valid := interfaceOut1.valid && interfaceOut2.valid && interfaceOut3.valid
+    ioout.valid := RegNext(interfaceOut1.valid && interfaceOut2.valid && interfaceOut3.valid) && ioout.ready
 
+    val nco_imag1 = Reg(SInt(16.W))
+    val nco_real1 = Reg(SInt(16.W))
+    val nco_imag2 = Reg(SInt(16.W))
+    val nco_real2 = Reg(SInt(16.W))
+    val nco_imag3 = Reg(SInt(16.W))
+    val nco_real3 = Reg(SInt(16.W))
+    
     when (interfaceOut1.fire() && interfaceOut2.fire() && interfaceOut3.fire()) {
       DspContext.withBinaryPointGrowth(0) {
-        val nco_imag1 = (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        val nco_real1 = (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        val nco_imag2 = (multiplyingFactor2.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut2.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        val nco_real2 = (multiplyingFactor2.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut2.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        val nco_imag3 = (multiplyingFactor3.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut3.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        val nco_real3 = (multiplyingFactor3.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut3.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        val nco_imag_out = (nco_imag1 +& nco_imag2) +& nco_imag3
-        val nco_real_out = (nco_real1 +& nco_real2) +& nco_real3
-        ioout.bits.data := Cat(nco_real_out.asTypeOf(UInt(16.W)), nco_imag_out.asTypeOf(UInt(16.W)))
+        nco_imag1 := (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
+        nco_real1 := (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
+        nco_imag2 := (multiplyingFactor2.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut2.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
+        nco_real2 := (multiplyingFactor2.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut2.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
+        nco_imag3 := (multiplyingFactor3.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut3.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
+        nco_real3 := (multiplyingFactor3.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut3.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
       }
     }
+
+    val nco_imag_out = (nco_imag1 +& nco_imag2) +& nco_imag3
+    val nco_real_out = (nco_real1 +& nco_real2) +& nco_real3
+    ioout.bits.data := Cat(nco_real_out.asTypeOf(UInt(16.W)), nco_imag_out.asTypeOf(UInt(16.W)))
 
     DecoupledQueue(1024, interfaceIn1, interfaceOut1) := interfaceIn1.bits
     DecoupledQueue(1024, interfaceIn2, interfaceOut2) := interfaceIn2.bits
     DecoupledQueue(1024, interfaceIn3, interfaceOut3) := interfaceIn3.bits
-
+    
+    val valid1 = RegInit(false.B)
+    val valid2 = RegInit(false.B)
+    val valid3 = RegInit(false.B)
+    when(ioin1.fire()) {valid1 := true.B}
+    when(ioin2.fire()) {valid2 := true.B}
+    when(ioin3.fire()) {valid3 := true.B}
+    val started = valid1 && valid2 && valid3
+    
+    val lastCnt = RegInit(UInt(11.W), 0.U)
     val last1 = RegInit(false.B)
     val last2 = RegInit(false.B)
     val last3 = RegInit(false.B)
-    when(ioin1.bits.last) {last1 := true.B}.elsewhen(RegNext(ioout.bits.last)) {last1 := false.B}
-    when(ioin2.bits.last) {last2 := true.B}.elsewhen(RegNext(ioout.bits.last)) {last2 := false.B}
-    when(ioin3.bits.last) {last3 := true.B}.elsewhen(RegNext(ioout.bits.last)) {last3 := false.B}
-    when(last1 && last2 && last3 && ioout.valid){ioout.bits.last := true.B}.otherwise {ioout.bits.last := false.B}
+    when(ioin1.bits.last) {last1 := true.B}.elsewhen((lastCnt === 0.U) && ioout.valid) {last1 := false.B}
+    when(ioin2.bits.last) {last2 := true.B}.elsewhen((lastCnt === 0.U) && ioout.valid) {last2 := false.B}
+    when(ioin3.bits.last) {last3 := true.B}.elsewhen((lastCnt === 0.U) && ioout.valid) {last3 := false.B}
+    
+    when(!(last1 && last2 && last3) && started && !ioout.fire()) {
+      lastCnt := lastCnt + 1.U
+    }.elsewhen((last1 && last2 && last3) && started && ioout.fire()) {
+      lastCnt := lastCnt - 1.U
+    }
+    when(last1 && last2 && last3 && ioout.valid && (lastCnt === 0.U)){ioout.bits.last := true.B}.otherwise {ioout.bits.last := false.B}
   }
 }
 
@@ -140,20 +163,33 @@ class AXIBridge1to1Multiplying(dataWidth: Int, csrAddress: AddressSet, beatBytes
     interfaceIn1.valid := ioin1.valid
     interfaceIn1.bits := ioin1.bits.data.asUInt()
     interfaceOut1.ready := ioout.ready && interfaceOut1.valid
-    ioout.valid := interfaceOut1.valid
-
+    ioout.valid := RegNext(interfaceOut1.valid) && ioout.ready
+  
+    val nco_imag1 = Reg(SInt(16.W))
+    val nco_real1 = Reg(SInt(16.W))
+  
     when (interfaceOut1.fire()) {
       DspContext.withBinaryPointGrowth(0) {
-        val nco_imag1 = (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        val nco_real1 = (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
-        ioout.bits.data := Cat(nco_real1.asTypeOf(UInt(16.W)), nco_imag1.asTypeOf(UInt(16.W)))
+        nco_imag1 := (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((dataWidth-1), 0).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
+        nco_real1 := (multiplyingFactor1.asTypeOf(FixedPoint(16.W, (dataWidth-2).BP)) context_* interfaceOut1.bits((2*dataWidth-1), dataWidth).asTypeOf(FixedPoint(dataWidth.W, (dataWidth-2).BP))).asSInt
       }
     }
+    ioout.bits.data := Cat(nco_real1.asTypeOf(UInt(16.W)), nco_imag1.asTypeOf(UInt(16.W)))
 
     DecoupledQueue(1024, interfaceIn1, interfaceOut1) := interfaceIn1.bits
     
+    val started = RegInit(false.B)
+    when(ioin1.fire()) {started := true.B}
+    
+    val lastCnt = RegInit(UInt(11.W), 0.U)
     val last1 = RegInit(false.B)
-    when(ioin1.bits.last) {last1 := true.B}.elsewhen(RegNext(ioout.bits.last)) {last1 := false.B}    
-    when(last1 && ioout.valid){ioout.bits.last := true.B}.otherwise {ioout.bits.last := false.B}
+    when(ioin1.bits.last) {last1 := true.B}.elsewhen((lastCnt === 0.U) && ioout.valid) {last1 := false.B}
+    
+    when(!last1 && started && !ioout.fire()) {
+      lastCnt := lastCnt + 1.U
+    }.elsewhen(last1 && started && ioout.fire()) {
+      lastCnt := lastCnt - 1.U
+    }
+    when(last1 && ioout.valid && (lastCnt === 0.U)){ioout.bits.last := true.B}.otherwise {ioout.bits.last := false.B}
   }
 }
